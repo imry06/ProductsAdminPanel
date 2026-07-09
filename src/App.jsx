5eb70bd4c3178c0ef7f1609dcd50a4f90336ec1b
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-09 01:40:06 -0400

    basic routing setups.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import './App.css'
-import AddProduct from './pages/AddProduct'
-import Dashboard from './pages/Dashboard'
-import EditProduct from './pages/EditProduct'
-import Products from './pages/Products'
+import Dashboard from "./pages/Dashboard";
+import AddProduct from "./pages/AddProduct";
+import EditProduct from "./pages/EditProduct";
+import Products from "./pages/Products";
 
 function App() {
-
   return (
-    <>
-         {/* <Dashboard/> */}
-         {/* <AddProduct/> */}
-         {/* <EditProduct/> */}
-         <Products/>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/add-product" element={<AddProduct />} />
+      <Route path="/edit-product/:id" element={<EditProduct />} />
+      <Route path="/products" element={<Products />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
