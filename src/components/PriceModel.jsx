c2ccf1f63e4ed0498870459f80ddb71e55251c47
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-09 00:20:01 -0400

    components added.

---
+++ src/components/PriceModel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PriceModel = () => {
+  return (
+    <div>PriceModel</div>
+  )
+}
+
+export default PriceModel
---
+++ src/components/StockModel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StockModel = () => {
+  return (
+    <div>StockModel</div>
+  )
+}
+
+export default StockModel
